26353c99bce281fee5190421ddb7c4a148d1e819
[Github <helgo1318@gmail.com>
2024-05-20 18:18:07 +0900

    feat: 작업 파일 합침

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onClick = () => {
+    if (!title.trim() || !description.trim()) {
+      alert("내용을 입력해주세요");
+      return;
+    }
+    const newValue = { title, description, id: Date.now, isDone: false };
+
+    setTodos((prev) => {
+      return [...prev, newValue];
+    });
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const onChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const onDeleteTodo = (deleteId) => {
+    // setTodos((todos) => {
+    //   return todos.filter => deleteId !== id
+    // })
+  };
+  const onChangeTodoState = (targetId) => {
+    setTodos((todos) => {
+      // return todos.map(())
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="header">
+        <div>todolist</div>
+        <div>react</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="form-container">
+        <input
+          type="text"
+          placeholder="제목"
+          onClick={() => onChangeTitle}
+          value={title}
+        />
+        <input
+          type="text"
+          placeholder="내용"
+          onClick={() => onChangeDescription}
+          value={description}
+        />
+        <div>
+          {todos.map(({ title, description, isDone, id }) => {
+            return <div key={id}>{title}</div>;
+          })}
+        </div>
+        <button onClick={onClick}>추가하기</button>
+      </div>
+
+      <div className="progress-container">
+        <div>working....</div>
+        <ul className="todos">
+          {todos.map(({ title, description, isDone, id }) => {
+            return (
+              <li className="todosCard" key={id}>
+                <div>{title}</div>
+                <div>{description}</div>
+                <button onClick={() => onDeleteTodo(id)}>삭제하기</button>
+                <button onClick={() => onChangeTodoState(id)}>완료</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="complete-container">
+        <div>done....</div>
+        <ul className="todos">
+          {todos.map(({ title, description, isDone, id }) => {
+            return (
+              <li className="todosCard" key={id}>
+                <div>{title}</div>
+                <div>{description}</div>
+                <button onClick={() => onDeleteTodo(id)}>삭제하기</button>
+                <button onClick={() => onChangeTodoState(id)}>완료</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
